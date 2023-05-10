f99db3109d1609a5f4b923cefa0b4a101557365a
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-05-10 18:27:29 -0400

    PF

---
+++ Portfolio.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest',
+            duration: 1000 // personaliza la duración de la animación a tu gusto
+        });
+    });
+});
