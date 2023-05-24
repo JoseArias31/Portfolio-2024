354a8aed99c634707a84981193cb594e640aae57
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-05-24 00:36:34 -0400

    PF

--- Portfolio.js
+++ Portfolio.js
@@ -1,11 +1,16 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-            inline: 'nearest',
-            duration: 1000 // personaliza la duración de la animación a tu gusto
-        });
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+        duration: 1000 // personaliza la duración de la animación a tu gusto
+      });
     });
-});
+  });
+
+
+  console.log("Hello, Jose")
+
+
