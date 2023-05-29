c7bd143ef419673e0007603039a01d2293b3c9c8
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-05-29 18:16:35 -0400

    PF

--- Portfolio.js
+++ Portfolio.js
@@ -1,4 +1,10 @@
+function showImage() {
+  document.getElementById("miModal").style.display = "block";
+}
 
+function closeModal() {
+  document.getElementById("miModal").style.display = "none";
+}
 
   console.log("Hello, Jose")
 
