1af9e9e77c83fd0f9fbe6b0bf8fa8a01bd189e11
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-06-02 01:28:51 -0400

    PF

--- Portfolio.js
+++ Portfolio.js
@@ -8,33 +8,7 @@ function closeModal(modalId) {
 
   console.log("Hello, Jose")
 
-  function openCodeWindow(exerciseNumber) {
-    var code = "";
-  
-    switch (exerciseNumber) {
-      case 1:
-        code = "console.log('Código del ejercicio 1');";
-        break;
-      case 2:
-        code = "console.log('Código del ejercicio 2');";
-        break;
-      case 3:
-        code = "console.log('Código del ejercicio 3');";
-        break;
-      case 4:
-        code = "console.log('Código del ejercicio 4');";
-        break;
-      case 5:
-        code = "console.log('Código del ejercicio 5');";
-        break;
-      default:
-        code = "No se encontró el ejercicio.";
-    }
-  
-    var estimateWindow = window.open("", "", "width=725,height=680");
-    estimateWindow.document.write("<pre>" + code + "</pre>");
-    estimateWindow.document.close();
-  }
+
   
   
 
