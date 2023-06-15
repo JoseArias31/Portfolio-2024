0711344cefc73a7160f8283504f7fc6b59b5855f
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-06-15 16:43:22 -0400

    PF

--- Portfolio.js
+++ Portfolio.js
@@ -9,10 +9,15 @@ function closeModal(modalId) {
   console.log("Hello, Jose")
 
 
-  
-  
+  document.getElementById('hireMe').addEventListener('click', function() {
+    var email = 'gojosearias@gmail.com';
+    var subject = 'Let\'s Work Together!';
+    var message = 'Hello,\n\nI came across your contact information and I am interested in discussing potential opportunities with you. I have attached my idea-proposal for your review.\n\nLooking forward to hearing from you soon!\n\nBest regards,\n[Your Name]\n[Phone Number]';
 
-  
+    var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(message);
+
+    window.open(mailtoLink);
+  });
   
   
   
