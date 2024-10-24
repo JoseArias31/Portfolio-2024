9ea69a8cb0cd913fa6eb9b055d54df52a76cebc7
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2024-10-23 23:24:05 -0400

    Enhancing Porfolio for a better online style.

--- Portfolio.js
+++ Portfolio.js
@@ -6,6 +6,40 @@ function closeModal(modalId) {
   document.getElementById(modalId).style.display = "none";
 }
 
+const text = "Full Stack Web Developer";
+let index = 0;
+let isDeleting = false; // Bandera para saber si estamos borrando el texto
+const speed = 100; // Velocidad de escritura en milisegundos
+const pauseBetweenLoops = 1000; // Tiempo de espera entre la eliminación y el inicio del nuevo ciclo
+
+function typeWriter() {
+  const typewriterElement = document.getElementById("typewriter");
+  
+  if (!isDeleting && index < text.length) {
+    // Si no estamos eliminando y el índice es menor que el texto, escribir letra por letra
+    typewriterElement.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  } else if (isDeleting && index > 0) {
+    // Si estamos eliminando, borramos letra por letra
+    typewriterElement.innerHTML = text.substring(0, index - 1);
+    index--;
+    setTimeout(typeWriter, speed / 2); // La eliminación puede ser más rápida
+  } else if (index === text.length && !isDeleting) {
+    // Cuando hemos terminado de escribir, esperamos antes de comenzar a borrar
+    isDeleting = true;
+    setTimeout(typeWriter, pauseBetweenLoops); 
+  } else if (isDeleting && index === 0) {
+    // Cuando hemos terminado de borrar, volvemos a comenzar el ciclo de escritura
+    isDeleting = false;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+// Iniciar la función
+typeWriter();
+
+
   console.log("Hello, Jose")
 
 
